95566dc0df9888c5def94e4d272fa22b5fb9fc78
Magnleik <magnleik@stud.ntnu.no>
2018-04-06 14:45:04 +0200

    created constructors for GameInstance, now implements Runnable

--- src/com/panic/tdt4240/GameInstance.java
+++ src/com/panic/tdt4240/GameInstance.java
@@ -1,13 +1,40 @@
 package com.panic.tdt4240;
 
+import java.net.Socket;
+import java.util.ArrayList;
+
 /**
  * Created by magnus on 06.04.2018.
  */
 
-public class GameInstance {
+public class GameInstance implements Runnable {
     private int ID;
+    private ArrayList<Socket> clients;
 
-    public GameInstance(int ID){
+    public GameInstance(int ID,Socket client){
         this.ID=ID;
+        if(clients==null){
+            clients = new ArrayList<>();
+        }
+        clients.add(client);
+    }
+    public GameInstance(Socket client){
+        if(clients==null){
+            clients = new ArrayList<>();
+        }
+        clients.add(client);
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public ArrayList<Socket> getClients() {
+        return clients;
+    }
+
+    @Override
+    public void run() {
+
     }
 }
--- src/com/panic/tdt4240/gameMakerThread/GameMaker.java
+++ src/com/panic/tdt4240/gameMakerThread/GameMaker.java
@@ -4,17 +4,19 @@ package com.panic.tdt4240.gameMakerThread;
 import com.panic.tdt4240.GameInstance;
 import com.panic.tdt4240.SorterThread;
 
+import java.net.Socket;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by magnus on 06.04.2018.
  */
 
-public class GameMaker {
+public class GameMaker implements Runnable{
     private static final AtomicInteger count = new AtomicInteger(0);
 
-    public GameMaker(){
-        GameInstance gameInstance= new GameInstance(count.incrementAndGet());
+    public GameMaker(Socket client){
+        GameInstance gameInstance= new GameInstance(count.incrementAndGet(),client);
         SorterThread.gameInstanceList.add(gameInstance);
+        new Thread(gameInstance).run();
     }
 }
