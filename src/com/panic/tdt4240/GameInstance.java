02abc39f3c72d728179bb6cef6c570f992cc7b61
Magnleik <magnleik@stud.ntnu.no>
2018-04-06 14:23:13 +0200

    created GameInstance class

---
+++ src/com/panic/tdt4240/GameInstance.java
@@ -0,0 +1,13 @@
+package com.panic.tdt4240;
+
+/**
+ * Created by magnus on 06.04.2018.
+ */
+
+public class GameInstance {
+    private int ID;
+
+    public GameInstance(int ID){
+        this.ID=ID;
+    }
+}
